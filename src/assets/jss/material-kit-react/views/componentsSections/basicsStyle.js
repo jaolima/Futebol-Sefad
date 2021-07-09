c0fe874d18423af30ec294c6302b8ee6a97b13c7
joaolimafonseca2@gmail.com <joao.fonseca@ipam.org.br>
2021-07-08 23:08:34 -0300

    start projec

---
+++ src/assets/jss/material-kit-react/views/componentsSections/basicsStyle.js
@@ -0,0 +1,34 @@
+import { container, title } from "assets/jss/material-kit-react.js";
+import customCheckboxRadioSwitch from "assets/jss/material-kit-react/customCheckboxRadioSwitch.js";
+
+const basicsStyle = {
+  sections: {
+    padding: "70px 0",
+  },
+  table: {
+    minWidth: 700,
+  },
+  container,
+  title: {
+    ...title,
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none",
+  },
+  space50: {
+    height: "50px",
+    display: "block",
+  },
+  space70: {
+    height: "70px",
+    display: "block",
+  },
+  icons: {
+    width: "17px",
+    height: "17px",
+    color: "#FFFFFF",
+  },
+  ...customCheckboxRadioSwitch,
+};
+
+export default basicsStyle;
---
+++ src/views/Components/Sections/SectionBasics.js
@@ -0,0 +1,186 @@
+import React from "react";
+// plugin that creates slider
+import Slider from "nouislider";
+// @material-ui/core components
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Radio from "@material-ui/core/Radio";
+import Switch from "@material-ui/core/Switch";
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+import People from "@material-ui/icons/People";
+import Check from "@material-ui/icons/Check";
+import FiberManualRecord from "@material-ui/icons/FiberManualRecord";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import CustomLinearProgress from "components/CustomLinearProgress/CustomLinearProgress.js";
+import Paginations from "components/Pagination/Pagination.js";
+import Badge from "components/Badge/Badge.js";
+
+import styles from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.js";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import Close from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(name, calories, fat, protein) {
+  return { name, calories, fat, protein };
+}
+
+const rows = [
+  createData('Brasileirão - Série D', '10/07 15:00', 'Brasiliense v Aparecidense'),
+  createData('Brasileirão - Série D', '18/07 15:30', 'SC Jaragua v Brasiliense' )
+];
+
+const useStyles = makeStyles(styles);
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+Transition.displayName = "Transition";
+
+
+export default function SectionBasics() {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([24, 22]);
+  const [selectedEnabled, setSelectedEnabled] = React.useState("b");
+  const [checkedA, setCheckedA] = React.useState(true);
+  const [checkedB, setCheckedB] = React.useState(false);
+  const [classicModal, setClassicModal] = React.useState(false);
+
+  const handleToggle = (value) => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    setChecked(newChecked);
+  };
+  return (
+    <div className={classes.sections}>
+      <div className={classes.container}>
+       
+        <div className={classes.title}>
+          <h2>Campeonatos</h2>
+        </div>
+        <div id="buttons">
+
+          <Dialog
+            classes={{
+              root: classes.center,
+              paper: classes.modal,
+            }}
+            open={classicModal}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={() => setClassicModal(false)}
+            aria-labelledby="classic-modal-slide-title"
+            aria-describedby="classic-modal-slide-description"
+          >
+            <DialogTitle
+              id="classic-modal-slide-title"
+              disableTypography
+              className={classes.modalHeader}
+            >
+              <IconButton
+                className={classes.modalCloseButton}
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={() => setClassicModal(false)}
+              >
+                <Close className={classes.modalClose} />
+              </IconButton>
+              <h4 className={classes.modalTitle}>Aqui podem aparecer algumas informações</h4>
+            </DialogTitle>
+            <DialogContent
+              id="classic-modal-slide-description"
+              className={classes.modalBody}
+            >
+              <p>
+               .....
+              </p>
+            </DialogContent>
+            <DialogActions className={classes.modalFooter}>
+              <Button
+                onClick={() => setClassicModal(false)}
+                color="danger"
+                simple
+              >
+                Fechar
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Campeonato</StyledTableCell>
+                  <StyledTableCell align="right">Data</StyledTableCell>
+                  <StyledTableCell align="right">Times</StyledTableCell>
+                  <StyledTableCell align="right"></StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right"><Button
+                      block
+                      onClick={() => setClassicModal(true)}
+                    >
+                      Visualizar
+                    </Button></StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+        </div>
+      </div>
+    </div>
+  );
+}
